4ef62554358774bc40f86fca44c91a4d74f0e54c
loganfin <finley4.logan@gmail.com>
2024-02-07 18:13:18 -0800

    Prep for PR

--- include/lumenaries/http/request.hpp
+++ include/lumenaries/http/request.hpp
@@ -4,32 +4,6 @@
 
 #include <string>
 
-// clang-format off
-
-/*
-// done
-
-// not done
-
-int httpd_req_to_sockfd(httpd_req_t *r);
-int httpd_req_recv(httpd_req_t *r, char *buf, size_t buf_len);
-esp_err_t httpd_req_get_hdr_value_str(httpd_req_t *r, const char *field, char *val, size_t val_size);
-esp_err_t httpd_req_get_url_query_str(httpd_req_t *r, char *buf, size_t buf_len);
-esp_err_t httpd_query_key_value(const char *qry, const char *key, char *val, size_t val_size);
-esp_err_t httpd_req_get_cookie_val(httpd_req_t *req, const char *cookie_name, char *val, size_t *val_size);
-
-// not doing
-
-// used when calling httpd_req_get_hdr_value_str()
-size_t httpd_req_get_hdr_value_len(httpd_req_t *r, const char *field);
-
-// used when calling httpd_req_get_url_query_str()
-size_t httpd_req_get_url_query_len(httpd_req_t *r);
-
-*/
-
-// clang-format on
-
 namespace lumenaries::http {
 
 /** \class Request http/Request lumenaries/http/Request
--- include/lumenaries/http/response.hpp
+++ include/lumenaries/http/response.hpp
@@ -4,32 +4,6 @@
 
 #include <string>
 
-// clang-format off
-/*
-// left to do
-
-// done
-
-esp_err_t httpd_resp_set_status(httpd_req_t *r, const char *status);
-esp_err_t httpd_resp_set_type(httpd_req_t *r, const char *type);
-esp_err_t httpd_resp_set_hdr(httpd_req_t *r, const char *field, const char *value);
-esp_err_t httpd_resp_send(httpd_req_t *r, const char *buf, ssize_t buf_len);
-esp_err_t httpd_resp_send_chunk(httpd_req_t *r, const char *buf, ssize_t buf_len);
-
-// not doing
-
-// send raw HTTP packet
-int httpd_send(httpd_req_t *r, const char *buf, size_t buf_len);
-
-// seems unnecessary since we already have set_status();
-esp_err_t httpd_resp_send_err(httpd_req_t *req, httpd_err_code_t error, const char *msg);
-
-// these just fill in the buf_len variable for httpd_resp_send() and httpd_resp_send_chunk()
-static inline esp_err_t httpd_resp_sendstr(httpd_req_t *r, const char *str);
-static inline esp_err_t httpd_resp_sendstr_chunk(httpd_req_t *r, const char *str);
-*/
-// clang-format on
-
 namespace lumenaries::http {
 
 /** \class Response http/Response lumenaries/http/Response
